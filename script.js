78400bf72ee8626cc0728e68623b8df04e072126
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-26 16:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let angle = 0;
 const line = document.getElementById('line');
 
 setInterval(() => {
-    angle += 2;  // Increment rotation angle
+    angle += 2;
     line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
 }, 20);
